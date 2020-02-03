44d9bb11ad00eb15c0e92b5f3006e1733df61dc1
rolandudv <u.roland58@gmail.com>
2020-02-03 21:47:07 +0000

    added footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,16 @@
         </main>
     </div>
 </body>
+
+<footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">Stuarts Balls &copy; <?php echo date ('Y')?>. All Rights Reserved</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+
 </html>
 
 
